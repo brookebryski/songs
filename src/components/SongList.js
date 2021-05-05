53db94e5fc4c28efe717c7e7fea73ab910bc9cd7
github username <github email address>
2021-05-05 17:03:43 -0500

    connected songlist

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends React.Component {
     render() {
-        return <div>SongList</div>
+        return <div>SongList</div>;
     }
 }
 
-export default SongList;
+export default connect()(SongList);
