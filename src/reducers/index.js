70d6fcf891db17036019c9a328e4d2e17e09150c
github username <github email address>
2021-05-05 16:34:46 -0500

    songs reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+    return [
+        { title: 'No Exit', duration: '3:55' },
+        { title: 'Runner', duration: '3:36' },
+        { title: 'How to Forgive', duration: '3:35' },
+        { title: 'Fields of Blue', duration: '3:28' }
+    ];
+};
